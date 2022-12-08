25c9963f3a7fea73e13772c003efdccd4e1e29ab
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-09 01:17:48 +0300

    fixed, did zadacha23

--- zadacha23/Program.cs
+++ zadacha23/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputNum() {
+    Console.Write("Input a number ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void OutputCube(int num) {
+    for(double i = 1; i <= num; i++) {
+        double cube = Math.Pow(i, 3);
+        Console.Write("{0} ", cube);
+    }
+}
+
+OutputCube(InputNum());
